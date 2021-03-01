8a750e5229b01e70e4d75ef7b1c5c165a890d588
mespinoza30 <m.espinoza16@hotmail.com>
2021-02-28 20:31:34 -0800

    CSS Company

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,15 +1,18 @@
+import './Company.css' //same folder as cmpany.js
 
 const Company = (props) => {
     const { student, ceo, employees, name, valuation, vehicles } = props.company; //props.company gives access to all values in app.js under setComapny.
 
     return (
-        <div>
-            <p>Student: {student}</p>
-            <p>Ceo: {ceo}</p>
-            <p>Employees: {employees}</p>
-            <p>Name: {name}</p>
-            <p>Valuation: {valuation}</p>
-            <p>Vehicles: {vehicles}</p>
+        <div className="company">
+            <ul>
+                <li>Student: {student}</li>
+                <li>Ceo: {ceo}</li>
+                <li>Employees: {employees}</li>
+                <li>Name: {name}</li>
+                <li>valuation: {valuation}</li>
+                <li>Vehicles: {vehicles}</li>
+            </ul>
         </div>
     )
 }
